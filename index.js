2d28993eedc59ef519cf34942d2d226a7eff715c
moulishankar6666 <moulishankar3669@gmail.com>
2024-11-28 21:02:05 +0530

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const express = require("express");
-// const dotenv = require("dotenv");
-const sqlite3 = require("sqlite3");
 const path = require("path");
-const { open } = require("sqlite");
 const cors = require("cors");
 
 //middleware to parse json requests
@@ -10,42 +7,15 @@ let db;
 const app = express();
 app.use(express.json());
 app.use(cors());
-// dotenv.config();
 
 const port = 3000;
 
-const connectToDB = async () => {
-  try {
-    db = await open({
-      filename: path.join(__dirname, "salarymanager.db"),
-      driver: sqlite3.Database,
-    });
-    console.log("DB is connected successfully");
-  } catch (error) {
-    console.error("DB Error :", error.message);
-    process.exit(1);
-  }
-};
-connectToDB();
-
 app.post("/add", async (req, res) => {
-  try {
-    const query = `INSERT INTO spends (spending_name,spending_type,amount) VALUES(?,?,?);`;
-    const response = await db.run(query, ["shopping", "House Expenses", 5000]);
-    res.json({ data: response });
-  } catch (err) {
-    console.error("Error", err.message);
-  }
+  res.json({ data: "working add api" });
 });
 
 app.get("/all", async (req, res) => {
-  try {
-    const query = `SELECT * FROM spends`;
-    const response = await db.all(query);
-    res.json({ data: response });
-  } catch (error) {
-    console.error(error.msg);
-  }
+  res.json({ data: "working all api" });
 });
 
 app.listen(port, (err) => {
