6b16188d905a30150acba704fc9dd28725ce0de0
moulishankar6666 <moulishankar3669@gmail.com>
2024-11-28 21:08:45 +0530

    get and post apis modified

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
+const sqlite3 = require("sqlite3");
+const { open } = require("sqlite");
 
 //middleware to parse json requests
 let db;
@@ -10,9 +12,7 @@ app.use(cors());
 
 const port = 3000;
 
-app.post("/add", async (req, res) => {
-  res.json({ data: "working add api" });
-});
+app.post("/add", async (req, res) => {});
 
 app.get("/all", async (req, res) => {
   res.json({ data: "working all api" });
