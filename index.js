fa5ef5bcd36bbb04e7d3382c1d44aeebb35c3b3a
moulishankar6666 <moulishankar3669@gmail.com>
2024-12-11 22:10:41 +0530

    developed new apis

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require("path");
 const cors = require("cors");
 const sqlite3 = require("sqlite3");
 const { open } = require("sqlite");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 //middleware to parse json requests
